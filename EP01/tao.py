41e293ae2f5ea599359ed260d72cb324a2cedc78
sitthichai <125616543+sitthichai-ntplc@users.noreply.github.com>
2023-02-19 10:00:56 +0700

    Add files via upload

---
+++ EP01/tao.py
@@ -0,0 +1,16 @@
+import turtle
+t = turtle.Pen()
+t.shape('turtle')
+t.clear()
+
+for i in range(5):
+    t.forward(285)
+    t.lt(144)
+t.rt(72)
+t.penup()
+t.pendown()
+t.circle(150)
+t.hideturtle()
+t.done()
+
+
